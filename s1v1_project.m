050a8a60dd5945460a34fb460a32c2b61bf6ea8e
Simon Weiler <simonw@ad.swc.ucl.ac.uk>
2021-11-08 20:06:48 +0000

    added new plotting script

--- s1v1_project.m
+++ s1v1_project.m
@@ -55,7 +55,7 @@ h1=histogram(all_eit,edges,'Normalization','probability');h1.FaceColor='k'
 hold on;h2=histogram(all_eif,edges,'Normalization','probability');h2.FaceColor='g'
 hold on;h3=histogram(all_eif2,edges,'Normalization','probability');h3.FaceColor='m'
 box off;xlabel('E/I ratio');ylabel('Relative counts');legend({'1 Hz','25 Hz','50 Hz'});
-hold on;plot([1 1],[0 0.25],'--k');legend boxoff 
+hold on;plot([1 1],[0 0.3],'--k');legend boxoff 
 %% Plotexmaple epsc/ipsc single pulse
 %cnr=22;
 cnr=22
@@ -161,17 +161,139 @@ fig4=figure;set(fig4, 'Position', [200, 800, 400, 200]);set(gcf,'color','w');
 subplot(1,2,1)
 plot(Ephys(temp(cnr)).sub_traces_highf(1:1*sr,3),'Color',[0.5 0.5 0.5],'LineWidth',1.2);set(gca,'box','off');
 hold on;plot(Ephys(temp(cnr)).sub_traces_highf(1:1*sr,2),'Color','b','LineWidth',1.2);set(gca,'box','off');
-
 hold on;plot([0.25*sr 0.25*sr],[ov_max ov_max],'Marker','v','MarkerFaceColor','c','MarkerEdgeColor','c');
 subplot(1,2,2)
-
 plot(Ephys(temp(cnr)).sub_traces_highf(1:1*sr,4),'Color',[0.5 0.5 0.5],'LineWidth',1.2);set(gca,'box','off');
 hold on;plot(Ephys(temp(cnr)).sub_traces_highf(1:1*sr,1),'Color','r','LineWidth',1.2);set(gca,'box','off');
-
 hold on;plot([0.25*sr 0.25*sr],[ov_max ov_max],'Marker','v','MarkerFaceColor','c','MarkerEdgeColor','c');
+%% IPSC quantification
+cl={'b',[0.5 0.5 0.5]};
+ttx_ipsc=[max(abs(Ephys(temp(3)).highf_p(:,2))) max(abs(Ephys(temp(3)).highf_p(:,3)));...
+    max(abs(Ephys(temp(2)).highf_p(:,2))) max(abs(Ephys(temp(2)).highf_p(:,3)));...
+  max(abs(Ephys(temp(1)).highf_p(:,1))) max(abs(Ephys(temp(1)).highf_p(:,3))) ];
+data=[];data=ttx_ipsc;
+fig3= figure;set(fig3, 'Name', 'Paired comp');set(fig3, 'Position', [200, 300, 200, 200]);set(gcf,'color','w');
+hold on;
+for i=1:length(data)
+     pl=plot([1,2],[data(:,1),data(:,2)],'color','k');    
+end
+hold on;pS=plotSpread([data(:,1),data(:,2)],'categoryIdx',[ones(1,length(data(:,1)))' ones(1,length(data(:,2)))'*2],...
+    'categoryMarkers',{'o','o'},'categoryColors',cl);hold on;
+box off;set(gca,'FontSize',10);
+ [p1]=signrank(data(:,1) ,data(:,2));p1=round(p1,3);
+title([' p=' num2str(p1) ', n=' num2str(length(data))],'FontWeight','Normal');
+xticklabels({'no TTX','TTX'});ylabel('IPSC amplitude (pA)');set(gca,'FontSize',10);
+%% EPSC
+ttx_epsc=[max(abs(Ephys(temp(3)).highf_n(:,1))) max(abs(Ephys(temp(3)).highf_n(:,4)));...
+    max(abs(Ephys(temp(2)).highf_n(:,1))) max(abs(Ephys(temp(2)).highf_n(:,4)));...
+  max(abs(Ephys(temp(1)).highf_n(:,3))) max(abs(Ephys(temp(1)).highf_n(:,4))) ];
+data=[];data=ttx_epsc;
+fig3= figure;set(fig3, 'Name', 'Paired comp');set(fig3, 'Position', [200, 300, 200, 200]);set(gcf,'color','w');
+hold on;
+for i=1:length(data)
+     pl=plot([1,2],[data(:,1),data(:,2)],'color','k');    
+end
+hold on;pS=plotSpread([data(:,1),data(:,2)],'categoryIdx',[ones(1,length(data(:,1)))' ones(1,length(data(:,2)))'*2],...
+    'categoryMarkers',{'o','o'},'categoryColors',cl);hold on;
+box off;set(gca,'FontSize',10);
+ [p1]=signrank(data(:,1) ,data(:,2));p1=round(p1,3);
+title([' p=' num2str(p1) ', n=' num2str(length(data))],'FontWeight','Normal');
+xticklabels({'no TTX','TTX'});ylabel('IPSC amplitude (pA)');set(gca,'FontSize',10);
+%% TTX modulation index
+par=[(ttx_ipsc(:,2)-ttx_ipsc(:,1))./(ttx_ipsc(:,2)+ttx_ipsc(:,1)); (ttx_epsc(:,2)-ttx_epsc(:,1))./(ttx_epsc(:,2)+ttx_epsc(:,1))]
+s1=[1:3];s2=[4:6]
+[statsout]=dual_barplot(par,s1,s2,2);xticks([1:1:2]);hold on;xticklabels({'IPSC' ,'EPSC'});ylabel('TTX modulation index');set(gca,'FontSize',10);xtickangle(45);
 %% 
-[max(abs(Ephys(temp(3)).highf_p(:,2))) max(abs(Ephys(temp(3)).highf_p(:,3)))]
-ttx_all_p=[ttx_p; ttx_214]
-ttx_all_p./max(ttx_all_p,[],2);
-p1=paired_plot(ttx_all_p,1,{'[0.5 0.5 0.5]','r'});xticklabels({'Before','After'});ylabel('Peak IPSC (pA)');set(gca,'FontSize',10);
+all_red=[];all_nred=[];
+all_red = cell_selecter(Ephys,'label',1,'pair',1,'geno',7);
+all_nred = cell_selecter(Ephys,'label',0,'pair',1,'geno',7);
+%% 
+all_red=[];all_nred=[];
+all_red = cell_selecter(Ephys,'label',1,'geno',7);
+all_nred = cell_selecter(Ephys,'label',0,'geno',7);
+%% 
+red_epsc=[];red_epscf=[];nred_epsc=[];nred_epscf=[];
+temp=[];
+for i=1:length(find(all_red==1));
+   temp=find(all_red==1);
+   red_epsc(i)=max(abs(Ephys(temp(i)).train_n(:)));
+end
+temp=[];
+   for i=1:length(find(all_nred==1));
+   temp=find(all_nred==1);
+   nred_epsc(i)=max(abs(Ephys(temp(i)).train_n(:)));
+   end
+   
+temp=[];
+for i=1:length(find(all_red==1));
+   temp=find(all_red==1);
+   red_epscf(i)=max(abs(Ephys(temp(i)).high_n(:)));
+end
+temp=[];
+   for i=1:length(find(all_nred==1));
+   temp=find(all_nred==1);
+   nred_epscf(i)=max(abs(Ephys(temp(i)).high_n(:)));
+   end
+   
+%    temp=[];
+% for i=1:length(find(all_red==1));
+%    temp=find(all_red==1);
+%    red_epschf(i)=max(abs(Ephys(temp(i)).highf_n(:)));
+% end
+% temp=[];
+%    for i=1:length(find(all_nred==1));
+%    temp=find(all_nred==1);
+%    nred_epschf(i)=max(abs(Ephys(temp(i)).highf_n(:)));
+%    end
+   
+   %% All against all
+   par=[red_epsc nred_epsc]
+   s1=1:length(red_epsc)
+   s2=length(red_epsc)+1:length(nred_epsc)+length(red_epsc)
+  [statsout]=dual_barplot(par,s1,s2,0);xticks([1:1:2]);hold on;xticklabels({'Cre+' ,'Cre-'});ylabel('EPSC Amplitude (pA)');set(gca,'FontSize',10);xtickangle(45);
 
+%% only using pairs
+cl={'r',[0.5 0.5 0.5]};
+data=[];data=[red_epscf' nred_epscf'];
+fig3= figure;set(fig3, 'Name', 'Paired comp');set(fig3, 'Position', [200, 300, 200, 200]);set(gcf,'color','w');
+hold on;
+for i=1:length(data)
+     pl=plot([1,2],[data(:,1),data(:,2)],'color','k');    
+end
+
+hold on;pS=plotSpread([data(:,1),data(:,2)],'categoryIdx',[ones(1,length(data(:,1)))' ones(1,length(data(:,2)))'*2],...
+    'categoryMarkers',{'o','o'},'categoryColors',cl);hold on;
+%hold on;plot([1,2],[nanmedian(data(:,1)),nanmedian(data(:,2))],'k','LineWidth',3);
+%hold on;plot([1,2],[nanmean(data(:,1)),nanmean(data(:,2))],'k','LineWidth',3);
+box off;set(gca,'FontSize',10);
+%hold on;errorbar([0.75 2.25],nanmean(data),nanstd(data,[],1)/sqrt(length(data)),'ok','MarkerFaceColor','r','Markersize',7);
+hold on;errorbar([0.75],nanmean(data(:,1)),nanstd(data(:,1),[],1)/sqrt(length(data(:,1))),'ok','MarkerFaceColor','r','Markersize',7);
+hold on;errorbar([2.25],nanmean(data(:,2)),nanstd(data(:,2),[],1)/sqrt(length(data(:,1))),'ok','MarkerFaceColor',[0.5 0.5 0.5],'Markersize',7);
+ [p1]=signrank(data(:,1) ,data(:,2));p1=round(p1,3);
+title([' p=' num2str(p1) ', n=' num2str(length(data))],'FontWeight','Normal');
+xticklabels({'Cre+','Cre-'});ylabel('EPSC Amplitude (pA)');set(gca,'FontSize',10);
+
+%% 
+
+cnr=6
+ov_min=-400;ov_max=600;
+temp=[];
+temp=find(all_red==1);
+fig4=figure;set(fig4, 'Position', [200, 200, 600, 300]);set(gcf,'color','w');
+subplot(1,2,1)
+plot(Ephys(temp(cnr)).sub_traces_high(1:2*sr,1),'Color','r','LineWidth',1);set(gca,'box','off');
+ylim([-200 20])
+ov_min=-400;ov_max=600;
+title('Cre+')
+temp=[];
+temp=find(all_nred==1);
+subplot(1,2,2)
+
+plot(Ephys(temp(cnr)).sub_traces_high(1:2*sr,1),'Color',[0.5 0.5 0.5],'LineWidth',1);set(gca,'box','off');
+ylim([-200 20])
+title('Cre-')
+%% 
+
+ plot(Ephys(temp(cnr)).sub_traces_train(1:1*sr,2),'Color','r','LineWidth',1);set(gca,'box','off');
+hold on;plot(Ephys(temp(cnr)).sub_traces_train(1:1*sr,1),'Color','b','LineWidth',1);set(gca,'box','off');hold on;ylim([ov_min-10 ov_max]);   
+hold on;plot([0.25*sr 0.25*sr],[ov_max ov_max],'Marker','v','MarkerFaceColor','c','MarkerEdgeColor','c');
